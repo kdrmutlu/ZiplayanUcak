b79edf813a9b2c6b2542d9e83cbb81cecb1041b1
Kadir Mutlu <kadirmutluu@gmail.com>
2016-06-27 16:02:16 +0300

    Proje oluşturuldu. Android Studio ve libGDX ilişkilendirildi.

---
+++ android/src/com/kmutlu/ziplayanucak/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kmutlu.ziplayanucak;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kmutlu.ziplayanucak.ZiplayanUcak;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ZiplayanUcak(), config);
+	}
+}
