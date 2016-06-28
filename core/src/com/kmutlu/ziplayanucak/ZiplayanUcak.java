56858e15535aa80b3891dcef0e36eca5d209267e
Kadir Mutlu <kadirmutluu@gmail.com>
2016-06-28 17:56:24 +0300

    Arkaplan eklendi.

--- core/src/com/kmutlu/ziplayanucak/ZiplayanUcak.java
+++ core/src/com/kmutlu/ziplayanucak/ZiplayanUcak.java
@@ -3,25 +3,47 @@ package com.kmutlu.ziplayanucak;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class ZiplayanUcak extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private SpriteBatch oyunSayfasi;
+	private OrthographicCamera hareketliKamera;
+	private Texture bgResmi;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		oyunSayfasi = new SpriteBatch();
+
+		hareketliKamera = new OrthographicCamera();
+		hareketliKamera.setToOrtho(false, 800, 480);
+
+		bgResmi = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		dunyayiCizdir();
+
+	}
+
+	private void dunyayiCizdir() {
+		hareketliKamera.update();
+
+		//oyun sayfasını hareketli kameraya ayarla
+		oyunSayfasi.setProjectionMatrix(hareketliKamera.combined);
+
+		oyunSayfasi.begin();
+
+		oyunSayfasi.draw(bgResmi, 0, 0);
+
+		oyunSayfasi.end();
 	}
 }
